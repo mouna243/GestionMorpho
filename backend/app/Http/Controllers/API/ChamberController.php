46469488fa821653aca38c975a8857f4e999ae86
mouna243 <mounasaoui2108@gmail.com>
2026-04-06 20:29:22 +0100

    finire le crud de chamber avec la gestion de suppression et modification des images

---
+++ backend/app/Http/Controllers/API/ChamberController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Chamber;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ChamberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chambers = Chamber::all();
+        return response()->json([
+            'data' => $chambers
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|string|max:255",
+            "image" => "required|image|mimes:jpg,png,jpeg|max:2048",
+            "description" => "required|string|max:500",
+            "is_available" => "required|boolean",
+            "chamber_type_id" => "required",
+        ]);
+
+        $file = $request->file('image');
+        $name = uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
+        $path = $file->storeAs('chamberImages', $name, 'public');
+
+        $chamber = Chamber::create([
+            "name" => $request->name,
+            "image" => $path,
+            "description" => $request->discription,
+            "is_available" => $request->is_available,
+            "chamber_type_id" => $request->chamber_type_id
+        ]);
+
+        if ($chamber) {
+            return response()->json([
+                "data" => $chamber,
+                "message" => "chamber created successfully",
+                "success" => true
+            ], 200);
+        }
+        return response()->json([
+            "message" => "chamber create failed",
+            "success" => false
+        ], 401);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $chamber = Chamber::find($id);
+
+        if ($chamber) {
+            return response()->json([
+                'data' => $chamber,
+                'success' => true
+            ]);
+        }
+        return response()->json([
+            'success' => false
+        ], 404);
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chamber $chamber)
+    {
+        $request->validate([
+            "name" => "required|string|max:255",
+            "image" => "image|mimes:jpg,png,jpeg|max:2048",
+            "description" => "required|string|max:500",
+            "is_available" => "required|boolean",
+            "chamber_type_id" => "required",
+        ]);
+
+
+        if ($request->hasFile("image")) {
+            if ($chamber->image && Storage::exists($chamber->image)) {
+                Storage::delete($chamber->image);
+            }
+            $file = $request->file('image');
+            $name = uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('chamberImages', $name, 'public');
+        } else {
+            $path = $chamber->image;
+        }
+
+        $is_updated = $chamber->update([
+            "name" => $request->name,
+            "image" => $path,
+            "description" => $request->discription,
+            "is_available" => $request->is_available,
+            "chamber_type_id" => $request->chamber_type_id
+        ]);
+
+        if (!$is_updated) {
+            return response()->json([
+                'message' => "chamber is not updated",
+                'success' => false
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => "chamber is updated",
+            'success' => true
+        ], 200);
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chamber $chamber)
+    {
+        $is_deleted = $chamber->delete();
+        if ($is_deleted) {
+            if ($chamber->image && Storage::exists($chamber->image)) {
+                Storage::delete($chamber->image);
+            }
+            return response()->json([
+                'message' => "chamber is deleted successfuly",
+                'success' => true
+            ],200);
+        }
+        return response()->json([
+            'message' => "chamber is not deleted ",
+            'success' => false
+        ],400);
+
+    }
+}
