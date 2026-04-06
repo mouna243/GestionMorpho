743740360706de995d337e0d2d1e948c6b53ed14
mouna243 <mounasaoui2108@gmail.com>
2026-04-06 20:28:12 +0100

    finir le crud pour chamber type

--- backend/app/Http/Controllers/API/ChamberTypeController.php
+++ backend/app/Http/Controllers/API/ChamberTypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\ChamberType;
 use Illuminate\Http\Request;
 
 class ChamberTypeController extends Controller
@@ -12,6 +13,11 @@ class ChamberTypeController extends Controller
      */
     public function index()
     {
+        //afficher tout les types de chambre
+        $chamberTypes = ChamberType::all();
+        return response()->json([
+            "data" => $chamberTypes
+        ], 200);
 
     }
 
@@ -20,30 +26,77 @@ class ChamberTypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            "name" => 'required|string|max:255',
+            "discription" => 'required|string|max:500',
+            "is_available" => 'required|boolean',
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        $chamberType = ChamberType::create([
+            'name' => $request->name,
+            'discription' => $request->discription,
+            'is_available' => $request->is_available
+        ]);
+        if (!$chamberType) {
+
+            return response()->json([
+                "success" => false,
+                "message" => "chamber type created failed"
+            ], 401);
+        }
+        return response()->json([
+            "data" => $chamberType,
+            "message" => "chamber type created successfully",
+            "success" => true
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, ChamberType $chamberType)
     {
-        //
+        $request->validate([
+            "name" => 'required|string|max:255',
+            "discription" => 'required|string|max:500',
+            "is_available" => 'required|boolean',
+        ]);
+
+        $is_updated = $chamberType->update([
+            'name' => $request->name,
+            'discription' => $request->discription,
+            'is_available' => $request->is_available
+        ]);
+
+        if ($is_updated) {
+            return response()->json([
+                "message" => " type chamber is updated ",
+                'success' => true
+            ]);
+        }
+
+        return response()->json([
+            "message" => " type chamber is not updated ",
+            'success' => false
+        ]);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(ChamberType $chamberType)
     {
-        //
+        $is_deleted = $chamberType->delete();
+        if (!$is_deleted) {
+            return response()->json([
+                'message' => " type chamber is deleted ",
+                'success' => true
+            ]);
+        }
+        return response()->json([
+            'message' => " type chamber is not deleted ",
+            'success' => false
+        ]);
     }
 }
