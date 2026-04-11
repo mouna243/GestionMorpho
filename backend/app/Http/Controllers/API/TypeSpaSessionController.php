b6eba4c2383a4c4715695c6065a28d7e8ab4586e
mouna243 <mounasaoui2108@gmail.com>
2026-04-11 15:06:11 +0100

    creation de type spa session controller

---
+++ backend/app/Http/Controllers/API/TypeSpaSessionController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\TypeSpaSession;
+use Illuminate\Http\Request;
+
+class TypeSpaSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //afficher tout les types de chambre
+        $typeSpaSessions = TypeSpaSession::all();
+        return response()->json([
+            "data" => $typeSpaSessions
+        ], 200);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'discription' => 'required|string|max:500',
+            'prix' => 'required|float',
+            'is_available' => 'required|boolean',
+        ]);
+
+        $typeSpaSession = TypeSpaSession::create([
+            'name' => $request->name,
+            'discription' => $request->discription,
+            'prix' => $request->prix,
+            'is_available' => $request->is_available
+        ]);
+        if (!$typeSpaSession) {
+
+            return response()->json([
+                "success" => false,
+                "message" => "type spa session created failed"
+            ], 401);
+        }
+        return response()->json([
+            "data" => $typeSpaSession,
+            "message" => "type spa session created successfully",
+            "success" => true
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TypeSpaSession $typeSpaSession)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'discription' => 'required|string|max:500',
+            'prix' => 'required|float',
+            'is_available' => 'required|boolean',
+        ]);
+
+        $is_updated = $typeSpaSession->update([
+            'name' => $request->name,
+            'discription' => $request->discription,
+            'prix' => $request->prix,
+            'is_available' => $request->is_available
+        ]);
+
+        if ($is_updated) {
+            return response()->json([
+                "message" => " type spa session is updated ",
+                'success' => true
+            ]);
+        }
+
+        return response()->json([
+            "message" => " type spa session is not updated ",
+            'success' => false
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeSpaSession $typeSpaSession)
+    {
+        $is_deleted = $typeSpaSession->delete();
+        if (!$is_deleted) {
+            return response()->json([
+                'message' => " type spa session is deleted ",
+                'success' => true
+            ]);
+        }
+        return response()->json([
+            'message' => " type spa session is not deleted ",
+            'success' => false
+        ]);
+    }
+}
