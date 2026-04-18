9ce3e16b5df67efa48d7ab304cb21ecfa53d3d03
mouna243 <mounasaoui2108@gmail.com>
2026-04-18 16:44:59 +0100

    ajouter un seeder pour un admin

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make(env('ADMIN_PASSWORD', 'password')),
+            'role' => 'admin',
+            'age' => 25,
+            'CIN' => 'K125478',
+            'telephon' => '+212 63214-5879',
+        ]);
+
+    }
+}
