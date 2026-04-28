abb472b21b2202653be17b0219717464bf1e3510
mouna243 <mounasaoui2108@gmail.com>
2026-04-28 09:33:31 +0100

    use the ApiTokens

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+     use HasApiTokens;
      protected $casts = [
         'experiences' => 'array',
         'langages' => 'array',
